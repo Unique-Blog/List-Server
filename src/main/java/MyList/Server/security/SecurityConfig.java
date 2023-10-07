824af2644977448853ec9db2bd0650251221222c
Wonbin Choi <dnjsqls5973@naver.com>
2023-10-07 23:54:53 +0900

    fix: csrf().disable() 해제(session 사용하기 위함)

--- src/main/java/MyList/Server/security/SecurityConfig.java
+++ src/main/java/MyList/Server/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http
                 .cors()
                 .and()
-                .csrf().disable()
+//                .csrf().disable()
 
                 .authorizeRequests()
                 .anyRequest().permitAll();
